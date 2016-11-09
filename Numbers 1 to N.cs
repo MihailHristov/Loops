9db2ceaea4d9903033e01545d94ddce36715303d
MihailHristov <mihail.hr7@gmail.com>
2016-11-09 08:20:26 +0200

    Add files via upload

---
+++ Numbers 1 to N.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)//pri n=4 izkarva 1,2,3,3 i t.n
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
